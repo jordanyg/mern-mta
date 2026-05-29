0f112bedcbef3c8a462585550c3f141bf0d8aa98
Yordanos Getachew <jordangetachew49@gmail.com>
2026-05-29 14:40:44 +0300

    project model

--- backend/controllers/orgController.js
+++ backend/controllers/orgController.js
@@ -1,84 +1,94 @@
 import Org from "../models/orgModel.js";
-import User from "../models/userModel.js";
-import asyncHandler from 'express-async-handler'
+import asyncHandler from "express-async-handler";
 import bcrypt from "bcryptjs";
 import Membership from "../models/memvershipModel.js";
 
+const createOrg = asyncHandler(async (req, res) => {
+    const { name, secret } = req.body;
 
-
-const createOrg = asyncHandler(async(req,res)=>{
-    const {name , secret} = req.body
-    if(!name || !secret){
-        res.status(400)
-        throw new Error('please add all fields')
-    }
-    const orgExists = await Org.findOne({name})
-
-    if(orgExists){
-        res.status(400)
-        throw new Error('organization already exists')
+    if (!name || !secret) {
+        res.status(400);
+        throw new Error("please add all fields");
     }
 
-    const salt = await bcrypt.genSalt(10)
-    const hashedSecret = await bcrypt.hash(secret , salt)
-    const loggedInUser = req.user
+    const orgExists = await Org.findOne({ name });
+
+    if (orgExists) {
+        res.status(400);
+        throw new Error("organization already exists");
+    }
+
+    const hashedSecret = await bcrypt.hash(secret, 10);
+
     const organization = await Org.create({
-        name , 
-        secret:hashedSecret,
-        user : req.user.id
-    })
+        name,
+        secret: hashedSecret,
+        user: req.user.id,
+    });
 
-    if(organization){
-        res.status(201).json({
-            organizationName : organization.name,
-            userName : loggedInUser.name,
-        })
-    }
-    if(!organization){
-        res.status(400)
-        throw new Error('it did not create')
-    }
-}
-)
-const getOrgs = async(req,res)=>{
-    const organizations = await Membership.find({user : req.user.id})
-    if(organizations){
-        res.status(200).json(organizations)
-    }else{
-        res.status(400)
-        throw new Error('no organizations found')
-    }
-}
+    await Membership.create({
+        user: req.user.id,
+        organization: organization._id,
+        membership: "owner",
+    });
 
-const joinOrg = async(req,res)=>{
-    const {secret , name} = req.body
-    if(!secret || !name){
-        res.status(400)
-        throw new Error('please enter fields')
+    res.status(201).json({
+        organizationName: organization.name,
+        userName: req.user.name,
+    });
+});
+
+const getOrgs = asyncHandler(async (req, res) => {
+    const organizations = await Membership.find({
+        user: req.user.id,
+    }).populate("organization");
+
+    res.status(200).json(organizations);
+});
+
+const joinOrg = asyncHandler(async (req, res) => {
+    const { secret, name } = req.body;
+
+    if (!secret || !name) {
+        res.status(400);
+        throw new Error("please enter fields");
     }
-    const organization = await Org.findOne({name})
+
+    const organization = await Org.findOne({ name });
+
+    if (!organization) {
+        res.status(404);
+        throw new Error("organization not found");
+    }
+
     const exists = await Membership.findOne({
-        user  : req.user.id,
-        organization  : organization._id
-    })
-    if(exists){
-        res.status(400)
-        throw new Error('already joined')
-    }
-    
-    if(organization && await bcrypt.compare(secret , organization.secret)){
-        await Membership.create({
-            user : req.user.id, 
-            organization : organization.id
-        })
+        user: req.user.id,
+        organization: organization._id,
+    });
 
-        res.status(200).json({
-            message : `successfully joined ${organization.name}`
-        })
-    }else{
-        res.status(400)
-        throw new Error('secret or name incorrect')
+    if (exists) {
+        res.status(400);
+        throw new Error("already joined");
     }
-}
 
-export {createOrg , getOrgs ,joinOrg}
+    const validSecret = await bcrypt.compare(
+        secret,
+        organization.secret
+    );
+
+    if (!validSecret) {
+        res.status(400);
+        throw new Error("secret incorrect");
+    }
+
+    await Membership.create({
+        user: req.user.id,
+        organization: organization._id,
+    });
+
+    res.status(200).json({
+        message: `successfully joined ${organization.name}`,
+    });
+});
+
+export { createOrg, getOrgs, joinOrg };
