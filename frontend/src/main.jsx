72a533ac0e6f57896c6172ec266f4e7ed173908e
Yordanos Getachew <jordangetachew49@gmail.com>
2026-06-04 14:03:50 +0300

    working on the frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
+import { Provider } from 'react-redux'
 import App from './App.jsx'
+import store from './store.js'
+
+
 
 createRoot(document.getElementById('root')).render(
-
+<Provider store={store}>
   <BrowserRouter>
     <StrictMode>
       <App />
     </StrictMode>
   </BrowserRouter>
-  
+</Provider>
 )
